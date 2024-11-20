4574bc3bb4275894706460fbda701a698552fca3
DreadLex <kazantsev.lex95@gmail.com>
2024-11-20 09:43:15 +0300

    Initial commit

---
+++ Purractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Purractice
+//
+//  Created by Alexander Kazantsev on 20.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
